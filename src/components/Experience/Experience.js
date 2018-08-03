c81461bace2c95b4ee0cb7aace8aaadd99bc1228
Taylor Sam <taylorsam209@yahoo.com>
2018-08-02 17:52:21 -0700

    Added react native to expertise, aligned the items more, fixed nav height, added React Native Video

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -8,26 +8,24 @@ class Experience extends Component {
       <div className="Experience">
         <h1>Experience</h1>
 
-        <div className="work-container">
+        <div className="work-experience-container">
           <h2>DevMountain Labs - React Native Developer</h2>
           <iframe
+          className="video-container"
             src="https://www.youtube.com/embed/i4AMHO-GFqk"
             // frameborder="0"
             allow="encrypted-media"
             allowfullscreen='allowfullscreen'
           />
-          <div>
-            <p>React Native Mobile Application </p>
-          </div>
-          <h3>Technology Used</h3>
-          <div className="description-container">
-            <p className="tech-description">
+          <h3>React Native Mobile Application</h3>
+      
+            <p >
               Mobile App for iOS and Android devices built with the React Native
               framework to allow customers of Metro Inc to earn reward points
               for recycling scrap metals, recieve coupons on metals every month,
               and stay connected to their local scrap metal recycler.
             </p>
-          </div>
+          
           <a
             href="https://github.com/taylorsam209/Metro-Rewards-Mobile-App"
             style={{ textDecoration: "none" }}
--- src/components/Expertise/Expertise.js
+++ src/components/Expertise/Expertise.js
@@ -15,11 +15,12 @@ class Expertise extends Component {
                     <li><i className="fab fa-react fa-2x"></i> ReactJS with Redux</li>
                     <li><i className="fab fa-node-js fa-2x"></i> Express</li>
                     <li><i className="fab fa-node fa-2x"></i> NodeJS</li>
+                    <li><i className="fab fa-react fa-2x"></i> React Native</li>
                     <li><i className="fas fa-database fa-2x"></i> PostgreSQL</li>
                     <li><i className="fas fa-cloud-upload-alt fa-2x"></i> RESTful APIs</li>
                     <li><i className="fab fa-git fa-2x"></i> Git Version Control</li>
                     <li><i className="fab fa-sass fa-2x"></i> SASS Styling</li>
-                    <li><i className="fas fa-code fa-2x"></i> Test Driven Development with Jest and Postman</li>
+                    <li><i className="fas fa-code fa-2x"></i> Test Driven Development</li>
                 </ul>
             </div>
         )
